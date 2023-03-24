ec3e46080336a8019e1510731148a6669d26b2ba
Kwentsir <kwentsirkwesi@gmail.com>
2023-03-24 01:10:01 +0000

    Update data

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\kwent\Desktop\Software Development\Park Coast\CorporateWebsite\venv
+set VIRTUAL_ENV=E:\Users\kwent\Desktop\Software Development\Park Coast\CorporateWebsite\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
